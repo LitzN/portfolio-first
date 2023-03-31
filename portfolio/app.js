98b8e15ceac1b26690797b5e56f7c6821cbaa865
LitzN <alitzianedyalkova@hotmail.co.uk>
2023-03-31 14:00:32 +0100

    Added second project, sneek peek and updated description

--- portfolio/app.js
+++ portfolio/app.js
@@ -1,17 +1,25 @@
 nav = document.getElementById('nav')
 proj1PeekBtn = document.getElementById('proj1PeekBtn')
+proj2PeekBtn = document.getElementById('proj2PeekBtn')
 proj1div = document.getElementById('proj-1')
+proj2div = document.getElementById('proj-2')
 proj1img = document.getElementById('proj1img')
+proj2img = document.getElementById('proj2img')
 navHeight = nav.offsetHeight;
 video1 = document.createElement('video')
+video2 = document.createElement('video')
 video1.src =
   './images/KetoMeals-fave.mp4';
-video1.controls = true;
-video1.muted = true;
-video1.autoplay = true;
-video1.height = 400;
-video1.width = 400;
+video2.src = './images/MovieDB.mp4'
+video2.controls = video1.controls = true;
+video2.muted = video1.muted = true;
+video2.autoplay = video1.autoplay = true;
+video2.height = video1.height = 400;
+video2.width = video1.width = 400;
 video1.classList.add('project-display')
+video2.classList.add('project-display')
+
+
 
 window.onscroll = function(e) {
     if (!(this.oldScroll > this.scrollY)){
@@ -32,6 +40,16 @@ proj1PeekBtn.addEventListener('click',()=>{
     } else if (proj1div.classList.contains('vid')){
         proj1div.replaceChild(proj1img,video1)
         proj1div.classList.replace('vid','img');
+    }
+})
+
+proj2PeekBtn.addEventListener('click',()=>{
+    if (proj2div.classList.contains('img')){
+        proj2div.replaceChild(video2,proj2img)
+        proj2div.classList.replace('img','vid');
+    } else if (proj2div.classList.contains('vid')){
+        proj2div.replaceChild(proj2img,video2)
+        proj2div.classList.replace('vid','img');
     }
 })
 
